87c2437beaaa307f845910b8eb5d84bdad51e5ce
Joshua Shew <joshua.t.shew@gmail.com>
2024-03-17 19:32:20 -0400

    Simplified process

--- fast-csv-subset-r/main.R
+++ fast-csv-subset-r/main.R
@@ -1,60 +1,57 @@
-# Setup
+# Setup ###########################################################################################
 
 library(peakRAM)
 library(pbapply)
 library(tidyverse)
 
+# Set default progress bar settings
 pbo <- pbapply::pboptions()
 
+# Constants controlling the experiment ------------------------------------------------------------
 height <- 1e4
 width <- 1e3
-num_portions <- 5
+num_portions <- 20
 step <- 1 / num_portions
 portions <- seq(1 - step * (num_portions - 1), 1, step)
+tmp_file_name <- "sample_data.csv"
 
-col_names <- function(width)
-  paste0("C", 1:width)
+# Create the sample data table --------------------------------------------------------------------
+write.table(
+  matrix(1:(height * width), height, width),
+  file = tmp_file_name,
+  row.names = FALSE,
+  sep = ","
+)
 
-col_subset <- function(width, portion) {
+# List of column names and getting an event subset of them based on the portion of the columns we
+# want to read.
+col_names <- paste0("V", 1:width)
+col_subset <- function(portion) {
   count <- portion * width
   step <- floor(1 / portion)
-  col_names(width)[seq(1, step * count, step)]
+  col_names[seq(1, step * count, step)]
 }
 
-file_name <- function(width)
-  paste0("sample-data_", width, ".csv")
+# Two methods of reading the data -----------------------------------------------------------------
 
-write_file <- function(width)
-  write.table(
-    matrix(1:(height * width), height, width),
-    file = file_name(width),
-    row.names = FALSE,
-    col.names = FALSE,
-    sep = ","
-  )
+# This one uses `col_select`
+read_with_col_select <- function(portion)
+  readr::read_csv(tmp_file_name,
+                  col_select = any_of(col_subset(portion)),
+                  show_col_types = FALSE)
 
-read_file_method_1 <- function(portion)
-  readr::read_csv(
-    file_name(width),
-    col_names = col_names(width),
-    col_select = col_subset(width, portion),
-    show_col_types = FALSE
-  )
+# This one selects a subset of the table after loading the whole thing
+read_then_select <- function(portion)
+  readr::read_csv(tmp_file_name,
+                  show_col_types = FALSE)[col_subset(portion)]
 
-read_file_method_2 <- function(portion)
-  readr::read_csv(file_name(width),
-                  col_names = col_names(width),
-                  show_col_types = FALSE)[col_subset(width, portion)]
+# Functions for benchmarking ----------------------------------------------------------------------
 
-read_file <- function(method_num, portion)
-  read_methods[[method_num]](portion)
-
-read_methods <- c(read_file_method_1, read_file_method_2)
-num_methods <- length(read_methods)
-
+# Peak RAM usage measurement
 measure_peak_ram <- function(expr)
   peakRAM::peakRAM(expr)$Peak_RAM_Used_MiB
 
+# Execution time measurement
 measure_exec_time <- function(expr) {
   start_time <- Sys.time()
   eval(expr)
@@ -62,22 +59,33 @@ measure_exec_time <- function(expr) {
   as.numeric(end_time - start_time)
 }
 
-# Generate files
-write_file(width)
+# Testing and Evaluating Results ##################################################################
 
-# 3. analyze the data (hypothesis testing)
+# Generate tests ----------------------------------------------------------------------------------
 
-# Generate tests
-num_rep <- 5
+# The number of times to repeat any combination of settings (reading method, portion)
+repetition_count <- 5
+
+# A collection of all reading methods to test
+reading_functions <- c(read_with_col_select, read_then_select)
+
+# Create a matrix of all combinations of portions and reading methods
+alternating_reading_methods <-
+  rep(1:length(reading_functions), length(portions))
+repeated_portions <- rep(portions, each = length(reading_functions))
 measurement_params <-
-  cbind(rep(1:(num_methods), length(portions)), rep(portions, each = num_methods))
+  cbind(alternating_reading_methods, repeated_portions)
+
+# Repeat the values based on the constant for the number of repetitions and randomize the order
 measurement_params <-
-  do.call(rbind, rep(list(measurement_params), num_rep))
+  do.call(rbind, rep(list(measurement_params), repetition_count))
 measurement_params <-
   measurement_params[sample(nrow(measurement_params)), ]
+
+# Create an expression to be evaluated for each test
 tests <- lapply(seq(nrow(measurement_params)), function(index) {
   read_file_expr <-
-    bquote(read_file(.(measurement_params[index, 1]), .(measurement_params[index, 2])))
+    bquote(reading_functions[[.(measurement_params[index, 1])]](.(measurement_params[index, 2])))
   bquote(c(
     .(index),
     .(measurement_params[index, 1]),
@@ -87,22 +95,22 @@ tests <- lapply(seq(nrow(measurement_params)), function(index) {
   ))
 })
 
-# Run tests
+# Run tests ---------------------------------------------------------------------------------------
 raw_mem_results <- pbapply::pblapply(tests, function(test) {
   eval(test)
 })
 
-# Processing results
+# Processing results ------------------------------------------------------------------------------
 mem_results <- do.call(rbind, raw_mem_results)
 dimnames(mem_results)[[2]] <-
   c("id", "method", "portion", "memory", "exec_time")
-mem_results <- as_tibble(as.data.frame(mem_results))
-mem_results <- mem_results %>%
-  mutate_at(vars(method), factor) %>% 
-  mutate(memory = memory / 1e6) %>% 
+mem_results <- as.data.frame(mem_results) %>%
+  as_tibble() %>% 
+  mutate_at(vars(method), factor) %>%
+  mutate(memory = memory / 1e6) %>%
   mutate(method = recode(method, `1` = "Optimized", `2` = "Default"))
 
-# Plotting results
+# Plotting results --------------------------------------------------------------------------------
 
 # Execution time
 ggplot(mem_results, aes(x = portion, y = exec_time, color = method)) +
